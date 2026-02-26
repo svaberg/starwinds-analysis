5604644580bfd2b8841ba505ac9ff66505b912d6
Dag Evensberget <dag.evensberget@gmail.com>
2026-02-26 07:30:06 +0100

    Add pipelines package boundary

---
+++ starwinds_analysis/pipelines/__init__.py
@@ -0,0 +1,8 @@
+"""THIS FILE defines the pipelines package boundary.
+
+Pipelines contain explicit multi-step workflows composed from library primitives.
+They should orchestrate reusable building blocks, but not replace notebooks/examples
+for one-off analysis code.
+"""
+
+__all__: list[str] = []
